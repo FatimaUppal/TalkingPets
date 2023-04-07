3de3905e944c350398bf4af7fd77700cf5b4ed48
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-04-06 20:06:25 -0400

    forget mainTest

--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -3,6 +3,33 @@ package io.zipcoder.polymorphism;
 import org.junit.Test;
 
 public class MainApplicationTest {
-@Test
-    public void testMe(){}
+    @Test
+    public void testArrayListNames(){
+        //Given
+        Dog dog = new Dog("Zack");
+        Cat cat = new Cat("Paul");
+        Bird bird = new Bird("King");
+
+        //When
+        ArrayList<Pet> pets = new ArrayList<>(Arrays.asList(dog, cat, bird));
+        String[] names = {"Zack", "Paul", "King"};
+
+        //Then
+        Assert.assertEquals(names, MainApplication.getUserPetNames(pets));
+    }
+
+    @Test
+    public void testArrayListSpeak(){
+        //Given
+        Dog dog = new Dog("Zack");
+        Cat cat = new Cat("Paul");
+        Bird bird = new Bird("King");
+
+        //When
+        ArrayList<Pet> pets = new ArrayList<>(Arrays.asList(dog, cat, bird));
+        String[] speak = {"Woof!", "Meow!", "Squawk!"};
+
+        //Then
+        Assert.assertEquals(speak , MainApplication.getUserPetSpeak(pets));
+    }
 }
